9e9a5241d8efeba74ddc0ce27ad7cd554a7b25d8
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-07-09 12:27:12 -0700

    created temp landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/images/Health Buddy - Logo.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <div class="header-container">
+            <div class="logo-container">
+                <img src={logo} alt ="Logo" />
+                <h1>
+                    Health Buddy
+                </h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#Login">Login</a></li>
+                    <li><a href="#Feedback">Feedback</a></li>
+                </ul>
+            </nav>
+        </div>
+      </header>
+      <section>
+      <div class="hero">
+            <div class="content">
+                <h1>
+                    Welcome to Health Buddy
+                </h1>
+                <h2>
+                    Take Charge Of Your Health
+                </h2>
+            </div>
+        </div>
+      </section>
     </div>
   );
 }
