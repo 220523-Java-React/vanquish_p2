41b9c15ef11c4ec80857d210aee8e41476bcef0a
Annie Kwon <annie.s.kwon@gmail.com>
2022-07-13 15:40:01 -0400

    added components folder

---
+++ src/components/NavMenu.js
@@ -0,0 +1,12 @@
+export default function NavMenu(){
+    return <>
+      <ul NavMenu>
+        <li>Body Info</li>
+        <li>Body Feedback</li>
+        <li>Minerals</li>
+        <li>Nutrients</li>
+        <li>Progress</li>
+        <li>Vitamins</li>
+      </ul>
+    </>
+  }
