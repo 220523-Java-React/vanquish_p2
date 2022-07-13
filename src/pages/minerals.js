afff0d71d4821e42879947c8884200e6e7c5f213
Annie Kwon <annie.s.kwon@gmail.com>
2022-07-13 17:13:58 -0400

    created nutriends and vitamins objects

---
+++ src/pages/minerals.js
@@ -0,0 +1,68 @@
+const choline = {
+    name: "Choline",
+    description: "Choline is an essential nutrient that is naturally present in some foods and available as a dietary supplement. Choline is a source of methyl groups needed for many steps in metabolism.",
+    url: "https://ods.od.nih.gov/factsheets/Choline-HealthProfessional/"
+}
+
+const magnesium = {
+    name: "Magnesium",
+    description: "Magnesium is an important part of the more than 300 enzymes found in your body. These enzymes are involved in processes that help to regulate many bodily functions, including the production of energy, body protein and muscle contractions.",
+    url: "https://www.eatright.org/food/vitamins-and-supplements/types-of-vitamins-and-nutrients/magnesium"
+}
+
+const calcium = {
+    name: "Calcium",
+    description: "Calcium "
+}
+
+const zinc = {
+
+}
+
+const iron = {
+
+}
+
+const copper = {
+
+}
+
+const selenium = {
+
+}
+
+const maganese = {
+
+}
+
+const phosphorus = {
+
+}
+
+const potassium = {
+
+}
+
+const iodine = {
+
+}
+
+const chromium = {
+
+}
+
+const molhybdenum = {
+
+}
+
+const flouride = {
+
+}
+
+const sodium = {
+
+}
+
+const chloride = {
+
+}
