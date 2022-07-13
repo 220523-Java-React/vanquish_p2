411f765cefc721b1276770694e20fb420c5b388d
Annie Kwon <annie.s.kwon@gmail.com>
2022-07-13 11:14:02 -0400

    Progress test

--- src/App.js
+++ src/App.js
@@ -1,39 +1,45 @@
 import './App.css';
 import logo from './assets/images/Health Buddy - Logo.png';
+import Progress from './progress.js'
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <div class="header-container">
+    <>
+      <Header />
+      <Progress />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <div className="App">
+        <header>
+          <div class="header-container">
             <div class="logo-container">
-                <img src={logo} alt ="Logo" />
-                <h1>
-                    Health Buddy
-                </h1>
+              <img src={logo} alt ="Logo" />
+              <h1>Health Buddy</h1>
             </div>
             <nav>
-                <ul>
-                    <li><a href="#Home">Home</a></li>
-                    <li><a href="#Login">Login</a></li>
-                    <li><a href="#Feedback">Feedback</a></li>
-                </ul>
+              <ul>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="#Login">Login</a></li>
+                <li><a href="#Feedback">Feedback</a></li>
+              </ul>
             </nav>
-        </div>
-      </header>
-      <section>
-      <div class="hero">
+          </div>
+        </header>
+        <section>
+          <div class="hero">
             <div class="content">
-                <h1>
-                    Welcome to Health Buddy
-                </h1>
-                <h2>
-                    Take Charge Of Your Health
-                </h2>
+              <h1>Welcome to Health Buddy</h1>
+              <h2>Take Charge Of Your Health</h2>
             </div>
-        </div>
-      </section>
-    </div>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
---
+++ src/progress.js
@@ -0,0 +1,8 @@
+function Progress(){
+    return 
+    <>
+        <h1>This is Progress</h1>
+    </>
+}
+
+export default Progress
