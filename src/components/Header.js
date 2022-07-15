792b0bd3face5e4a902ad17d1283df52e4c15a72
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-07-14 19:17:18 -0700

    set header based on conditional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,70 +1,16 @@
-import { Link as RouterLink } from 'react-router-dom';
-import {
-  Grid,
-  Box,
-  Typography,
-  Link as MaterialLink
-} from '@mui/material';
+import { useLocation } from 'react-router-dom'; 
 
-import logo from '../assets/images/Health Buddy - Logo.png';
+import HeaderMain from '../components/HeaderMain';
 
 export default function Header() {
+
+  const location = useLocation();
+  const pageHeader = (location.pathname === '/') ? <HeaderMain /> : 'other page';
+
   return (
     <>
       <header>
-        <Box class='header-container'>
-          <Box class='logo-container' >
-            <img src={logo} alt ='Logo' />
-            <Typography variant='h1' style={{fontFamily: 'Times New Roman, Times, serif', fontWeight: 'bold', fontSize: '50px', marginLeft: '20px'}}>
-              Health Buddy
-            </Typography>
-          </Box>
-          
-          <nav>
-            <Grid container justifyContent='space-evenly' alignItems='center' spacing={10}>
-              
-              <Grid item>
-                <MaterialLink
-                  to='/'
-                  component={RouterLink}
-                  underline='none'
-                  variant='body1'
-                >
-                  <Typography style={{fontSize: '18px', fontWeight: 'bold', textTransform: 'uppercase', color: 'black'}}>
-                    Home
-                  </Typography>
-                </MaterialLink>
-              </Grid>
-
-              <Grid item>
-                <MaterialLink
-                  to='/Login'
-                  component={RouterLink}
-                  underline='none'
-                  variant='body1'
-                >
-                  <Typography style={{fontSize: '18px', fontWeight: 'bold', textTransform: 'uppercase', color: 'black'}}>
-                    Login
-                  </Typography>
-                </MaterialLink>
-              </Grid>
-
-              <Grid item>
-              <MaterialLink
-                  to='/body-feedback'
-                  component={RouterLink}
-                  underline='none'
-                  variant='body1'
-                >
-                  <Typography style={{fontSize: '18px', fontWeight: 'bold', textTransform: 'uppercase', color: 'black'}}>
-                    Feedback
-                  </Typography>
-                </MaterialLink>
-              </Grid>
-            </Grid>
-
-          </nav>
-        </Box>
+        {pageHeader}
       </header>
     </>
   );
