d4f5c0227922a79a9cc9197ddb53f5ab66fe1bd6
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-07-14 17:40:56 -0700

    seperated hero section from Header to Home

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,23 @@ import LoginForm from "./LoginForm";
 import logo from '../assets/images/Health Buddy - Logo.png';
 
 export default function Header() {
-    return (
-      <>
-        <div className="App">
-          <header>
-            <div class="header-container">
-              <div class="logo-container" >
-                <img src={logo} alt ="Logo" />
-                <h1>Health Buddy</h1>
-              </div>
-              <nav>
-                <ul>
-                  <li><a href="#Home">Home</a></li>
-                  <li><LoginForm /></li>
-                  <li><a href="#Feedback">Feedback</a></li>
-                </ul>
-              </nav>
-            </div>
-          </header>
-          <section>
-            <div class="hero">
-              <div class="content">
-                <h1>Welcome to Health Buddy</h1>
-                <h2>Take Charge Of Your Health</h2>
-              </div>
-            </div>
-          </section>
+  return (
+    <>
+      <header>
+        <div class="header-container">
+          <div class="logo-container" >
+            <img src={logo} alt ="Logo" />
+            <h1>Health Buddy</h1>
+          </div>
+          <nav>
+            <ul>
+              <li><a href="#Home">Home</a></li>
+              <li><LoginForm /></li>
+              <li><a href="#Feedback">Feedback</a></li>
+            </ul>
+          </nav>
         </div>
-      </>
-    );
-  }
+      </header>
+    </>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-        Home
-    </div>
+    <section>
+      <div class="hero">
+        <div class="content">
+          <h1>Welcome to Health Buddy</h1>
+          <h2>Take Charge Of Your Health</h2>
+        </div>
+      </div>
+    </section>
   )
 };
 
