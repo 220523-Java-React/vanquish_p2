55f1ce9729e7acb4483140cc85ec8b478dd1e1c0
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-07-15 18:01:04 -0700

    bug fixed: Minerals.js not found

--- src/pages/minerals.js
+++ src/pages/minerals.js
@@ -1,48 +1,80 @@
 import NavMenu from '../components/NavMenu';
 import { minerals } from '../utils/ArrayFacts';
-import { Typography } from '@mui/material';
-
-
+import { 
+  Typography,
+  Box,
+  Link
+} from '@mui/material';
 
 const Minerals = () => {
   return (
     <>
       <div style={{fontSize: '200px', marginTop: '100px'}}>Minerals</div>
-        <a href={minerals[0].url}>Choline</a>
-        <Typography>{minerals[0].description}</Typography><br></br>
-        <a href={minerals[0].url}>Magnesium</a>
-        <Typography>{minerals[1].description}</Typography><br></br>
-        <a href={minerals[0].url}>Calcium</a>
-        <Typography>{minerals[2].description}</Typography><br></br>
-        <a href={minerals[0].url}>Zinc</a>
-        <Typography>{minerals[3].description}</Typography><br></br>
-        <a href={minerals[0].url}>Copper</a>
-        <Typography>{minerals[4].description}</Typography><br></br>
-        <a href={minerals[0].url}>Selenium</a>
-        <Typography>{minerals[5].description}</Typography><br></br>
-        <a href={minerals[0].url}>Manganese</a>
-        <Typography>{minerals[6].description}</Typography><br></br>
-        <a href={minerals[0].url}>Phosphorus</a>
-        <Typography>{minerals[7].description}</Typography><br></br>
-        <a href={minerals[0].url}>Potassium</a>
-        <Typography>{minerals[8].description}</Typography><br></br>
-        <a href={minerals[0].url}>Iodine</a>
-        <Typography>{minerals[9].description}</Typography><br></br>
-        <a href={minerals[0].url}>Chromium</a>
-        <Typography>{minerals[10].description}</Typography><br></br>
-        <a href={minerals[0].url}>Molhybdenum</a>
-        <Typography>{minerals[11].description}</Typography><br></br>
-        <a href={minerals[0].url}>Fluoride  </a>
-        <Typography>{minerals[12].description}</Typography><br></br>
-        <a href={minerals[0].url}>Sodium chloride</a>
-        <Typography>{minerals[13].description}</Typography><br></br>
-        <a href={minerals[0].url}>Choline</a>
-        <Typography>{minerals[14].description}</Typography><br></br>
-       
+        
+        <section>
+          <Box>
+            <Link href={minerals[0].url}>Choline</Link>
+            <Typography style={{marginBottom: '10px'}}>{minerals[0].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Magnesium</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[1].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Calcium</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[2].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Zinc</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[3].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Copper</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[4].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Selenium</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[5].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Manganese</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[6].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Phosphorus</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[7].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Potassium</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[8].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Iodine</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[9].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Chromium</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[10].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Molhybdenum</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[11].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Fluoride  </Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[12].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Sodium chloride</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[13].description}</Typography>
+          </Box>
+          <Box>
+            <Link href={minerals[0].url}>Choline</Link>
+            <Typography style={{marginBottom: '20px'}}>{minerals[14].description}</Typography>
+          </Box>
+        </section>       
+
       <NavMenu />
-      
-     
- 
       
     </>
   );
