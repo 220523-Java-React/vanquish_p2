c8488955caa5013152427b2749c5905f904d9384
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-07-21 12:47:49 -0700

    setup basic feedback table layout

--- src/pages/BodyFeedback.js
+++ src/pages/BodyFeedback.js
@@ -7,6 +7,7 @@ import {
 } from '@mui/material';
 
 import NavMenu from '../components/NavMenu';
+import { textAlign } from '@mui/system';
 
 const BodyFeedback = ({results, name}) => {
 
@@ -35,7 +36,7 @@ const BodyFeedback = ({results, name}) => {
                     padding: '20px 25px', 
                     borderRadius: '5px',
                     // border: '2px solid ', 
-                    color: 'lightblue',
+                    color: 'lightlightblue',
                     fontWeight: 'bold'
                   }}
                 >
@@ -62,26 +63,117 @@ const BodyFeedback = ({results, name}) => {
         </h2>
         <p>Here's your feedback based on the results you've recieved on the body info page.</p>
         
-        <Box>
-            GENERAL Recommendations  
+        <Box style={{display: 'flex', justifyContent: 'center', margin: '40px 0'}}>
+          <Grid container justifyContent='center' style={{border: '2px solid black', width: '50%'}}>
+            <Grid item xs={12} style={{background: 'orange', border: '2px solid black', textAlign: 'center'}}>
+              General Recommendations
+            </Grid>
+            <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+              BMI
+            </Grid>
+            <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+              BMI Read goes here
+            </Grid>
+            <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+              BF
+            </Grid>
+            <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+              BF Read goes here
+            </Grid>
+            <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+              WHE
+            </Grid>
+            <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+              WHE Read goes here
+            </Grid>
+            <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+              WHI
+            </Grid>
+            <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+              WHI Read goes here
+            </Grid>
+          </Grid>
+
         </Box>
 
-        <Grid container justifyContent='space-between'>
+        <Grid container justifyContent='space-between' spacing={5}>
           <Grid item xs={12} lg={4}>
-            <Grid container justifyContent='center'>
-              Vitamin Recommendations
+            <Grid container justifyContent='center' style={{border: '2px solid black'}}>
+              <Grid item xs={12} style={{background: 'orange', border: '2px solid black', textAlign: 'center'}}>
+                Vitamin Recommendations
+              </Grid>
+              <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+                VITAMIN A
+              </Grid>
+              <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+                500mg
+              </Grid>
+              <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+                VITAMIN B
+              </Grid>
+              <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+                1500mg
+              </Grid>
+              <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+                VITAMIN C
+              </Grid>
+              <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+                2500mg
+              </Grid>
             </Grid>
           </Grid>
           <Grid item xs={12} lg={4}>
-            <Grid container justifyContent='center'>
-              Nutrient Recommendations
+            <Grid container justifyContent='center' style={{border: '2px solid black'}}>
+            <Grid item xs={12} style={{background: 'orange', border: '2px solid black', textAlign: 'center'}}>
+                Macronutrient Recommendations
+              </Grid>
+              <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+                Calories
+              </Grid>
+              <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+                2000
+              </Grid>
+              <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+                Water
+              </Grid>
+              <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+                100 oz
+              </Grid>
+              <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+                Fat
+              </Grid>
+              <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+                500g
+              </Grid>
             </Grid>
           </Grid>
           <Grid item xs={12} lg={4}>
-            <Grid container justifyContent='center'>
-              Mineral Recommendations
+            <Grid container justifyContent='center' style={{border: '2px solid black'}}>
+              <Grid item xs={12} style={{background: 'orange', border: '2px solid black', textAlign: 'center'}}>
+                Mineral Recommendations
+              </Grid>
+              <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+                Choline
+              </Grid>
+              <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+                2000
+              </Grid>
+              <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+                Mineral 2
+              </Grid>
+              <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+                100 
+              </Grid>
+              <Grid item xs={6} style={{background: 'lightblue', border: '2px solid black'}}>
+                Mineral 3
+              </Grid>
+              <Grid item xs={6} style={{background: 'pink', border: '2px solid black'}}>
+                500
+              </Grid>
             </Grid>
           </Grid>
+          
+          
         </Grid>
       </section>
 
